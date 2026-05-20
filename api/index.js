8470b4ea388f761426c492e013bdbeba90f044a5
Wesley Silva Gomes <118454287+WesDevss@users.noreply.github.com>
2026-05-20 14:47:10 -0300

    fix(vercel): adiciona index.js como entrypoint do projeto

--- api/index.js
+++ api/index.js
@@ -1,6 +1,1 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
-const app = require('../backend/src/app');
-
-module.exports = app;
+module.exports = require('../index.js');
