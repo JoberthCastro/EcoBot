6d4eff9082e6a074fc44a023aabf80a090a6d52d
Wesley Silva Gomes <118454287+WesDevss@users.noreply.github.com>
2026-05-20 15:24:03 -0300

    fix(vercel): server.js com @vercel/node e lazy load, remove serverless-http

--- index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-
-const serverless = require('serverless-http');
-const createExpressApp = require('./backend/src/createExpressApp');
-
-module.exports = serverless(createExpressApp);
